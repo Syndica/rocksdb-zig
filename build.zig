340017f4cba7d2aa965b1a01432a27c8de402c83
InKryption <inkryption07@gmail.com>
2024-07-19 01:04:52 +0200

    Refactor & fix linker errors on linux

--- build.zig
+++ build.zig
@@ -1,96 +1,37 @@
 const std = @import("std");
-
-const ResolvedTarget = std.Build.ResolvedTarget;
+const Build = std.Build;
+const ResolvedTarget = Build.ResolvedTarget;
 const OptimizeMode = std.builtin.OptimizeMode;
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *Build) void {
     const target: ResolvedTarget = b.standardTargetOptions(.{});
     const optimize: OptimizeMode = b.standardOptimizeOption(.{});
 
-    const lib = buildRocksDb(b, target, optimize);
-    const mod = exposeRocksDb(b, target, optimize, lib);
-    buildRocksDbBindings(b, target, optimize, mod);
-}
+    const test_step = b.step("test", "Run bindings tests");
 
-/// Build the C++ library
-pub fn buildRocksDb(
-    b: *std.Build,
-    target: ResolvedTarget,
-    optimize: OptimizeMode,
-) *std.Build.Step.Compile {
-    // build rocksdb with make
-    // TODO: remove make dependency by reimplementing in zig
     const rocks_dep = b.dependency("rocksdb", .{});
-    const rocks_path = rocks_dep.path("").getPath(b);
-    const build_rocks, const librocksdb_a = addMakeAndMove(b, rocks_path, "static_lib", "librocksdb.a");
-    const build_bz2, const libbz2_a = addMakeAndMove(b, rocks_path, "libbz2.a", "libbz2.a");
-    const build_z, const libz_a = addMakeAndMove(b, rocks_path, "libz.a", "libz.a");
-    const DEBUG_LEVEL = switch (optimize) {
-        .Debug => "1",
-        else => "0",
-    };
-    build_rocks.setEnvironmentVariable("DEBUG_LEVEL", DEBUG_LEVEL);
-    build_bz2.setEnvironmentVariable("DEBUG_LEVEL", DEBUG_LEVEL);
-    build_z.setEnvironmentVariable("DEBUG_LEVEL", DEBUG_LEVEL);
 
-    // create static library
-    const lib = b.addStaticLibrary(.{
-        .name = "rocksdb",
-        .root_source_file = null,
-        .target = target,
-        .optimize = optimize,
-    });
-    lib.linkLibCpp();
-    lib.installHeadersDirectory(rocks_dep.path("include"), "", .{});
-
-    lib.step.dependOn(&build_rocks.step);
-    lib.step.dependOn(&build_bz2.step);
-    lib.step.dependOn(&build_z.step);
-
-    lib.addObjectFile(librocksdb_a);
-    lib.addObjectFile(libbz2_a);
-    lib.addObjectFile(libz_a);
-
-    b.installArtifact(lib);
-
-    return lib;
-}
-
-/// Directly expose the C++ library as a zig module
-pub fn exposeRocksDb(
-    b: *std.Build,
-    target: ResolvedTarget,
-    optimize: OptimizeMode,
-    lib: *std.Build.Step.Compile,
-) *std.Build.Module {
-    const rocks_dep = b.dependency("rocksdb", .{});
     const translate_c = b.addTranslateC(.{
         .root_source_file = rocks_dep.path("include/rocksdb/c.h"),
         .target = target,
         .optimize = optimize,
-        .link_libc = false,
     });
-    const mod = translate_c.addModule("rocksdb");
-    mod.link_libcpp = true;
-    mod.linkLibrary(lib);
+    const rocksdb_mod = b.addModule("rocksdb", .{
+        .root_source_file = translate_c.getOutput(),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+        .link_libcpp = true,
+    });
+    buildAndLinkRocksDb(b, rocksdb_mod);
 
-    return mod;
-}
-
-/// Build the zig bindings library, which is a wrapper for the C++ library
-pub fn buildRocksDbBindings(
-    b: *std.Build,
-    target: ResolvedTarget,
-    optimize: OptimizeMode,
-    rocksdb: *std.Build.Module,
-) void {
     // module
-    var bindings = b.addModule("rocksdb-bindings", .{
+    const bindings_mod = b.addModule("rocksdb-bindings", .{
         .target = target,
         .optimize = optimize,
         .root_source_file = b.path("src/lib.zig"),
     });
-    bindings.addImport("rocksdb", rocksdb);
+    bindings_mod.addImport("rocksdb", rocksdb_mod);
 
     // tests
     const tests = b.addTest(.{
@@ -98,27 +39,68 @@ pub fn buildRocksDbBindings(
         .optimize = optimize,
         .root_source_file = b.path("src/lib.zig"),
     });
-    tests.root_module.addImport("rocksdb", rocksdb);
+    tests.root_module.addImport("rocksdb", rocksdb_mod);
+    buildAndLinkRocksDb(b, &tests.root_module);
+
     const run_tests = b.addRunArtifact(tests);
-    const test_step = b.step("test", "Run bindings tests");
     test_step.dependOn(&run_tests.step);
+}
+
+/// Build and link the C++ library.
+fn buildAndLinkRocksDb(
+    b: *Build,
+    /// Module to add the object files to.
+    mod: *Build.Module,
+) void {
+    const make_and_move_exe = makeAndMoveExe(b);
+
+    // build rocksdb with make
+    // TODO: remove make dependency by reimplementing in zig
+    const rocks_dep = b.dependency("rocksdb", .{});
+    const rocks_path = rocks_dep.path("");
+
+    const librocksdb_a = addMakeAndMove(b, make_and_move_exe, rocks_path, "static_lib", "librocksdb.a");
+    const libbz2_a = addMakeAndMove(b, make_and_move_exe, rocks_path, "libbz2.a", "libbz2.a");
+    const libz_a = addMakeAndMove(b, make_and_move_exe, rocks_path, "libz.a", "libz.a");
+
+    mod.addIncludePath(rocks_dep.path("include"));
+
+    mod.addObjectFile(librocksdb_a);
+    mod.addObjectFile(libbz2_a);
+    mod.addObjectFile(libz_a);
+
+    mod.linkSystemLibrary("zstd", .{});
+    mod.linkSystemLibrary("lz4", .{});
+    mod.linkSystemLibrary("snappy", .{});
+    mod.linkSystemLibrary("uring", .{});
+}
+
+fn makeAndMoveExe(b: *Build) *Build.Step.Compile {
+    return b.addExecutable(.{
+        .name = "make-and-move",
+        .root_source_file = b.path("scripts/make_and_move.zig"),
+        .target = b.resolveTargetQuery(.{}),
+    });
 }
 
 /// add a build step to make the library and then move the output file to the cache
 /// the purpose is to have the zig caching system track the output file
 fn addMakeAndMove(
-    b: *std.Build,
-    rocks_path: []const u8,
+    b: *Build,
+    make_and_move_exe: *Build.Step.Compile,
+    rocks_lp: Build.LazyPath,
     make_rule: []const u8,
     filename: []const u8,
-) struct {
-    *std.Build.Step.Run,
-    std.Build.LazyPath,
-} {
-    const run = b.addSystemCommand(&.{
-        "zig",      "run",     b.path("scripts/make_and_move.zig").getPath(b), "--",
-        rocks_path, make_rule, filename,
-    });
-    const path = run.addOutputFileArg(filename);
-    return .{ run, path };
+) Build.LazyPath {
+    const run = b.addRunArtifact(make_and_move_exe);
+    run.setEnvironmentVariable("DEBUG_LEVEL", if (b.verbose) "1" else "0");
+
+    run.addDirectoryArg(rocks_lp);
+    run.addArg(make_rule);
+    run.addArg(filename);
+    if (b.verbose) {
+        run.stdio = .inherit;
+    }
+
+    return run.addOutputFileArg(filename);
 }
